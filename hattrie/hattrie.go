b9372fb2674d4ea2fc16803191d1a5b69a4ebbe2
Roman TSovanyan <covrom@gmail.com>
2019-02-20 22:46:37 +0300

    hattrie

--- hattrie/hattrie.go
+++ hattrie/hattrie.go
@@ -1,2 +1,14 @@
 package hattrie
 
+const (
+	// set the default number of slots in each container
+	HASH_SLOTS uint64 = 512
+)
+
+func bitwiseHash(b []byte) uint32 {
+	h := uint64(220373)
+	for _, c := range b {
+		h ^= (h << 5) + uint64(c) + (h >> 2)
+	}
+	return uint32((h & 0x7fffffff) & (HASH_SLOTS - 1))
+}
