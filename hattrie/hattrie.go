372d2eaf64c7335a003edf30aa36cea5282b4ba6
Roman TSovanyan <covrom@gmail.com>
2019-02-21 09:32:49 +0300

    hattrie

--- hattrie/hattrie.go
+++ hattrie/hattrie.go
@@ -19,9 +19,10 @@ func bitwiseHash(b []byte) uint32 {
 	return uint32((h & 0x7fffffff) & (HASH_SLOTS - 1))
 }
 
-type hashTable [][]byte
+type hashTable [HASH_SLOTS][]byte
 
-func resizeArray(ht hashTable, idx, arrayOffset, requiredIncrease uint32) hashTable {
+func resizeArray(ht hashTable, idx, arrayOffset, requiredIncrease uint32) {
+	// FIXME: append
 	if arrayOffset == 0 {
 		if requiredIncrease <= _32_BYTES {
 			ht[idx] = make([]byte, _32_BYTES)
@@ -32,22 +33,16 @@ func resizeArray(ht hashTable, idx, arrayOffset, requiredIncrease uint32) hashTa
 	} else {
 		oldArraySize := arrayOffset + 1
 		newArraySize := arrayOffset + requiredIncrease
-		// if the new array size can fit within the previously allocated 32-byte block,
-		// then no memory needs to be allocated.
 		if oldArraySize <= _32_BYTES && newArraySize <= _32_BYTES {
-			return ht
+			return
 		} else if oldArraySize <= _32_BYTES && newArraySize <= _64_BYTES {
-			// if the new array size can fit within a 64-byte block, then allocate only a
-			// single 64-byte block.
 			tmp := make([]byte, _64_BYTES)
 			copy(tmp, ht[idx][:oldArraySize])
 			ht[idx] = tmp
-			return ht
+			return
 		} else if oldArraySize <= _64_BYTES && newArraySize <= _64_BYTES {
-			// if the new array size can fit within a 64-byte block, then return
-			return ht
+			return
 		} else {
-			// resize the current array by as many 64-byte blocks as required
 			numberOfBlocks := ((oldArraySize - 1) >> 6) + 1
 			numberOfNewBlocks := ((newArraySize - 1) >> 6) + 1
 			if numberOfNewBlocks > numberOfBlocks {
@@ -57,34 +52,76 @@ func resizeArray(ht hashTable, idx, arrayOffset, requiredIncrease uint32) hashTa
 			}
 		}
 	}
-	return ht
 }
 
 func hashLookup(ht hashTable, query []byte) bool {
 	i := bitwiseHash(query)
-	if i >= uint32(len(ht)) {
+	if ht[i] == nil {
 		return false
 	}
 	array := ht[i]
-	for {
-		if len(array) < 3 {
-			break
-		}
+	for len(array) > 1 && array[0] != 0 {
 		// calculate the length of the current string in the array.
 		// Up to the first two bytes can be used to store the length of the string
 		ln := uint32(array[0])
 		if ln >= 128 {
 			ln = ((ln & 0x7f) << 8) | uint32(array[1])
+			array = array[2:]
+		} else {
+			array = array[1:]
 		}
-		array = array[2:]
 		word := array[:ln]
 		if bytes.Equal(word, query) {
 			return true
 		}
-		if uint32(len(array)) <= ln {
-			break
-		}
 		array = array[ln:]
 	}
 	return false
+}
+
+func hashInsert(ht hashTable, query []byte) bool {
+	// get the required slot.
+	idx := bitwiseHash(query)
+	if ht[idx] != nil {
+		array := ht[idx]
+		for len(array) > 1 && array[0] != 0 {
+			// calculate the length of the current string in the array.
+			// Up to the first two bytes can be used to store the length of the string
+			ln := uint32(array[0])
+			if ln >= 128 {
+				ln = ((ln & 0x7f) << 8) | uint32(array[1])
+				array = array[2:]
+			} else {
+				array = array[1:]
+			}
+			word := array[:ln]
+			if bytes.Equal(word, query) {
+				return false
+			}
+			array = array[ln:]
+		}
+	}
+	lnq := uint32(len(query))
+	lnadd := lnq + 2
+	if lnq >= 128 {
+		lnadd++
+	}
+	arroff := uint32(len(ht[idx]))
+	resizeArray(ht, idx, arroff, lnadd)
+	array := ht[idx]
+	if lnq < 128 {
+		array[arroff] = byte(lnq)
+		array = array[1:]
+	} else {
+		array[arroff] = byte(lnq>>8) | 0x80
+		array[arroff+1] = byte(lnq)
+		array = array[2:]
+	}
+	copy(array, query)
+	// FIXME: переделать на переменные длины массивов и append с cap, кратным кэшлайну
+	return true
+}
+
+func newTrie() {
+
 }
